6961791e40b7e984facbc37f7d3aeb27956dddc2
Denys iElkin <hvoarang@gmail.com>
2023-05-30 22:19:27 +0300

    hw05

---
+++ middlewares/index.js
@@ -0,0 +1,4 @@
+const validateBody = require("./validateBody");
+const authenticate = require("./authenticate");
+const upload = require("./upload");
+module.exports = { validateBody, authenticate, upload };
